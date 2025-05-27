f25c70541a81affc76dc6818d621100f4a09a1bd
A. K. M. Nafiz Ibn Nasim <akm.nafiz.ibn.nasim.bd@gmail.com>
2025-05-27 20:06:46 +0600

    Add files via upload

---
+++ Project1/src/main/java/com/mycompany/project1/Project1.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.project1;
+
+public class Project1 {
+
+    public static void main(String[] args) {
+       MyClass ObjectVariable= new MyClass();
+       ObjectVariable.myfunction();
+       ObjectVariable.myfullname();
+    }
+}

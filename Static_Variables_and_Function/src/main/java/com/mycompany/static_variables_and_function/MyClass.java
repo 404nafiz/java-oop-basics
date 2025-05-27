378113f9444dacd63abd11eb6050f5b65557322c
A. K. M. Nafiz Ibn Nasim <akm.nafiz.ibn.nasim.bd@gmail.com>
2025-05-27 20:27:39 +0600

    Add files via upload

---
+++ Static_Variables_and_Function/src/main/java/com/mycompany/static_variables_and_function/MyClass.java
@@ -0,0 +1,9 @@
+package com.mycompany.static_variables_and_function;
+public class MyClass {
+    public static int a=10;
+    public static void add()
+    {
+        int c=a+10;
+        System.out.println(c);
+    }
+}
---
+++ Static_Variables_and_Function/src/main/java/com/mycompany/static_variables_and_function/Static_Variables_and_Function.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.static_variables_and_function;
+
+public class Static_Variables_and_Function {
+
+    public static void main(String[] args) {
+        System.out.println(MyClass.a);
+        MyClass.add();
+       
+    }
+}

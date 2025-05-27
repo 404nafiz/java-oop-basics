1a3ea4bd758f25f3cbd7e4ba411b95e70cb71355
A. K. M. Nafiz Ibn Nasim <akm.nafiz.ibn.nasim.bd@gmail.com>
2025-05-27 20:08:34 +0600

    Add files via upload

---
+++ Instance_Variables/src/main/java/com/mycompany/instance_variables/Instance_Variables.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.instance_variables;
+
+/**
+ *
+ * @author User
+ */
+public class Instance_Variables {
+
+    public static void main(String[] args) {
+        MyClass MyClassObject= new MyClass();
+       System.out.println(MyClassObject.a);
+       System.out.println(MyClassObject.b);
+        MyClassObject.add();
+    
+        
+    }
+    
+    
+    
+}
---
+++ Instance_Variables/src/main/java/com/mycompany/instance_variables/MyClass.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.instance_variables;
+
+/**
+ *
+ * @author User
+ */
+public class MyClass {
+    
+    public int a=10 ;
+    public int b=12;
+    public int c;
+    public void add(){
+        this.c=this.a+this.b;
+        System.out.println(this.c);
+       
+    }
+    
+}

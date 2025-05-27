0706bd221c8a6158dc5aad28a3132f3d45c26d3f
A. K. M. Nafiz Ibn Nasim <akm.nafiz.ibn.nasim.bd@gmail.com>
2025-05-27 20:08:00 +0600

    Add files via upload

---
+++ Function_Parameter/src/main/java/com/mycompany/function_parameter/AddClass.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.function_parameter;
+
+
+public class AddClass {
+    
+    
+    public void add(int a,int b){
+       
+        int c=a+b;
+        System.out.println(c);
+    }
+    
+    public void check_even_odd(int a){
+        if(a%2==0) System.out.println("YES");
+        else System.out.println("NO"); 
+    } 
+}
---
+++ Function_Parameter/src/main/java/com/mycompany/function_parameter/Function_Parameter.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.function_parameter;
+
+public class Function_Parameter {
+
+    public static void main(String[] args) {
+        AddClass Objectadd= new AddClass();
+        Objectadd.add(3,10);
+        Objectadd.check_even_odd(4);
+    }
+}

146c45f8f6ba3ff9b1fab51283369406268962b3
A. K. M. Nafiz Ibn Nasim <akm.nafiz.ibn.nasim.bd@gmail.com>
2025-05-27 20:28:19 +0600

    Add files via upload

---
+++ Inheritance/src/main/java/com/mycompany/inheritance/Inheritance.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.inheritance;
+public class Inheritance {
+
+    public static void main(String[] args) {
+       Son sonobj=new Son();
+       sonobj.add();// son object dara fathher er jinish nia jai 
+       System.out.println(sonobj.a);
+       
+       
+       
+       /* ei sonobj a father er same function ta son k oo dia hoise tai son jeita or kase ase oita ee print korse orthat (-) korse kintu jodi son k same function dia na hoto tahole ans 30 hoto mane (+) hoto */
+    }
+}

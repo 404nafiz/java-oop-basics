09961323d035f24d93d1aecfc3fbedef317ba7bd
A. K. M. Nafiz Ibn Nasim <akm.nafiz.ibn.nasim.bd@gmail.com>
2025-05-28 21:59:31 +0600

    Add files via upload

---
+++ Constructor/src/main/java/com/mycompany/constructor/Constructor.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.constructor;
+
+public class Constructor {
+
+    public static void main(String[] args) {
+    Myclass obj= new Myclass(10);/* new er pore jeti ase oita ee conostructor method . conostructor method a object dia call korte hoi na and conostructor method er nam class name ee hobe */
+    
+    
+    /* normal method a return type thake but constructor method a return type thake na */
+    
+    
+    obj.demo();/* Normal method k call korar jonno object use kora hoi*/
+    /* conostructor overlode same ager moto kore ee kora hoi */
+    
+    
+    
+    }
+}
---
+++ Constructor/src/main/java/com/mycompany/constructor/Myclass.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.constructor;
+
+public class Myclass {
+    
+    Myclass(int a){
+        System.out.println("This is Conostructor Method and the value is ");System.out.print(a);
+    }
+    public void demo(){
+        System.out.println("This is Normal Method");
+    }
+    
+}

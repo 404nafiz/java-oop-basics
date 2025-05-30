d4dabde88879aacf008d04c250a524c62f98a2b6
A. K. M. Nafiz Ibn Nasim <akm.nafiz.ibn.nasim.bd@gmail.com>
2025-05-30 14:09:42 +0600

    Add files via upload

---
+++ Java_Interface/src/main/java/com/mycompany/java_interface/Java_Interface.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.java_interface;
+
+public class Java_Interface {
+
+    public static void main(String[] args) {
+       NewClass obj= new NewClass();
+       obj.FirstMethod();
+    }
+}
---
+++ Java_Interface/src/main/java/com/mycompany/java_interface/NewClass.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.java_interface;
+
+interface First{
+ void FirstMethod();
+
+}
+
+
+public class NewClass  implements First{
+    
+   public void  FirstMethod(){
+        System.out.println("First Interface");
+    }
+    
+}
